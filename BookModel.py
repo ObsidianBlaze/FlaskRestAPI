3702c70ba8debdcea74a1c40d575bc5a5a0c30cf
Idada Osaigbovo James <42479950+ObsidianBlaze@users.noreply.github.com>
2021-09-18 15:33:08 +0100

    Adding a book to the database along its isbn, price and name

--- BookModel.py
+++ BookModel.py
@@ -16,3 +16,26 @@ class Book(db.Model):
     name = db.Column(db.String(80), nullable = False)
     price = db.Column(db.Float, nullable = False)
     isbn = db.Column(db.Integer)
+
+    # Adding a book
+    def add_book(_name, _price, _isbn):
+        newBook = Book(name = _name, price = _price, isbn = _isbn)
+        # Adding the new book to the session
+        db.session.add(newBook)
+
+        # Committing the new book to the database
+        db.session.commit()
+
+    # Getting all books
+    def get_all_books():
+        return Book.query.all()
+
+    # Beautifying the structure of the output
+    def __repr__(self):
+        book_object = {
+            'name': self.name,
+            'price': self.price,
+            'isbn': self.isbn
+        }
+        # Changing the dictionary to json
+        return json.dumps(book_object)
